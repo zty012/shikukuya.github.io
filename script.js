1131ab9547683bfbe34e01e75fb29712d0e148fb
shikukuya <89395757+shikukuya@users.noreply.github.com>
2022-06-24 16:34:24 +0800

    .

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function getCookie(name) {
+  var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+  if (arr = document.cookie.match(reg)) {
+    return unescape(arr[2]);
+  } else {
+    return null;
+  }
+}
+
 function checkLanguage() {
-  window.location.href = "//baidu.com"
+  alert(getCookie("1"))
 }
