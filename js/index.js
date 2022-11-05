de01f46af25db04e0099b337550542721f3c0459
shikukuya <shikukuya@qq.com>
2022-11-05 17:51:22 +0800

    finish

--- js/index.js
+++ js/index.js
@@ -1,10 +1,81 @@
+function shuffle(arr) {
+  var a = arr;
+  a.sort((a, b) => {
+    return Math.random() > 0.5 ? -1 : 1;
+  });
+  return a;
+}
+
 function init($) {
   $("#background").warpDrive({
     width: innerWidth,
     height: innerHeight,
-    starCount: 2333,
+    autoResize: true,
+    starCount: 4455,
     starSpeedMax: 30,
   });
+  $("#splash").html(
+    shuffle([
+      "啊这",
+      "但凡有20树脂",
+      "原来你也玩原神"
+    ])[0]
+  );
+  $(document).on("scroll", (e) => {
+    if (
+      (document.body.scrollTop || document.documentElement.scrollTop)
+      == 0
+    ) $("#totop").addClass("hide");
+    else $("#totop").removeClass("hide");
+  })
+  $("#totop").click((e) => {
+    $(document).scrollTop(0);
+  })
+  $(".student").click((e) => {
+    alert(
+      "这个功能使用了一段“非常简单”的代码：" + `
+
+        var map = {
+          1: "一年级",
+          2: "二年级",
+          3: "三年级",
+          4: "四年级",
+          5: "五年级",
+          6: "六年级",
+          7: "初一",
+          8: "初二",
+          9: "初三",
+        };
+        var year = new Date().getFullYear().toString();
+        // 2018是一年级上学期的年份
+        var grade = year - 2018 + 1;
+        $(".student").html(map[grade]);
+    `
+    );
+  });
+  var map = {
+    1: "一年级",
+    2: "二年级",
+    3: "三年级",
+    4: "四年级",
+    5: "五年级",
+    6: "六年级",
+    7: "初一",
+    8: "初二",
+    9: "初三",
+  };
+  var year = new Date().getFullYear().toString();
+  var grade = year - 2018 + 1;
+  $(".student").html(map[grade]);
+  if (typeof $("html").data().darkreaderMode !== "undefined") {
+    $("body *").remove();
+    $("body").html(`
+      <p>请禁用darkreader，然后刷新页面</p>
+      <p>disable darkreader then refresh this page</p>
+      <img src="/img/disable_darkreader/1.png" alt="1">
+      <img src="/img/disable_darkreader/2.png" alt="2">
+    `)
+  }
 }
 
 $(init);
