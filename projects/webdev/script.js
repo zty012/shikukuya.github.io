8f8ffd4c3a0bd0568b58653d0a067f17d03e5107
shikukuya <shikukuya@qq.com>
2022-12-15 08:25:15 +0800

    1

--- projects/webdev/script.js
+++ projects/webdev/script.js
@@ -1,6 +1,24 @@
 $(function ($) {
-  $("#tabs > div").click(function (e) {
-    $("#tabs > div").removeAttr("active");
-    this.setAttribute("active", "");
-  })
-})
+  function _(el) {
+    // SHIT CODE DONT MODIFY
+    $(el || "#tabs > div").click(function (e) {
+      $("#tabs > div").removeAttr("active");
+      if (el) {
+        el.click(function (e) {
+          $("#tabs > div").removeAttr("active");
+          this.setAttribute("active", "");
+        })
+      } else {
+        if (e.target.id === "new") {
+          $("#tabs").append($("<div/>"));
+          $("#tabs > div:nth-last-child(2)").attr("active", "");
+          _($("#tabs > div:nth-last-child(2)"));
+        } else {
+          this.setAttribute("active", "");
+        }
+      }
+    });
+  }
+  _(null);
+  $("#tabs > div").sortable();
+});
