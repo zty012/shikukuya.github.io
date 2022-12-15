262d1561a8d76ac0c508cbf56eae9355a79c35ba
shikukuya <shikukuya@qq.com>
2022-12-15 13:30:17 +0800

    大改2

--- js/index.js
+++ js/index.js
@@ -1,50 +1,108 @@
 $(() => {
     let current = 1;
-    let timer_page = 0;
-    // 这里用jquery就无法获取wheelDelta了
-    $("body")[0].addEventListener("mousewheel", (e) => {
-        clearTimeout(timer_page);
-        timer_page = setTimeout(() => {
-            e = e || window.event;
-            e.wheelDelta = e.wheelDelta || e.detail;
-            if (e.wheelDelta > 0) {
-                // prev
-                prev();
-            }
-            if (e.wheelDelta < 0) {
-                // next
-                next();
-            }
-        }, 100);
-    });
-    $("#prev").on("click", (e) => {
-        prev();
-    });
-    $("#next").on("click", (e) => {
-        next();
-    });
-    $("#menu").on("click", (e) => {
-        menu();
-    });
-    $("#body > div:not(#body > div:first-child)").hide();
-
-    const next = () => {
+    let animate_speed = 8;
+    let animate_speed_temp = animate_speed;
+    let animate_speed_change = 0.01;
+    let animating = false;
+    const sleep = (delay) =>
+        new Promise((resolve) => setTimeout(resolve, delay));
+    const next = async () => {
         console.log("next");
-        if (!(current >= $("#body > div").length)) {
+        if (!(current >= $("#body > div").length) && !animating) {
+            animating = true;
+            animate_speed_temp = animate_speed;
             current++;
             $("#body > div").hide();
             $(`#body > div:nth-child(${current})`).show();
+            $("body").append($("#body").clone().attr("id", "body2"));
+            $("#body2").css("top", innerHeight + "px");
+            while (true) {
+                if (~~$("#body2").css("top").split("px")[0] <= 50) {
+                    break;
+                } else {
+                    $("#body").css(
+                        "top",
+                        ~~$("#body").css("top").split("px")[0] -
+                            animate_speed +
+                            "px"
+                    );
+                    $("#body2").css(
+                        "top",
+                        ~~$("#body2").css("top").split("px")[0] -
+                            animate_speed +
+                            "px"
+                    );
+                    animate_speed = animate_speed - animate_speed_change;
+                    await sleep(0.1);
+                }
+            }
+            $("#body").remove();
+            $("#body2").attr("id", "body");
+            animate_speed = animate_speed_temp;
+            animating = false;
         }
     };
-    const prev = () => {
+    const prev = async () => {
         console.log("prev");
-        if (!(current <= 1)) {
+        if (!(current <= 1) && !animating) {
+            animating = true;
+            animate_speed_temp = animate_speed;
             current--;
             $("#body > div").hide();
             $(`#body > div:nth-child(${current})`).show();
+            $("body").append($("#body").clone().attr("id", "body2"));
+            $("#body2").css("top", "-" + (innerHeight - 120) + "px");
+            while (true) {
+                if (~~$("#body2").css("top").split("px")[0] >= 50) {
+                    break;
+                } else {
+                    $("#body").css(
+                        "top",
+                        ~~$("#body").css("top").split("px")[0] +
+                            animate_speed +
+                            "px"
+                    );
+                    $("#body2").css(
+                        "top",
+                        ~~$("#body2").css("top").split("px")[0] +
+                            animate_speed +
+                            "px"
+                    );
+                    animate_speed = animate_speed - animate_speed_change;
+                    await sleep(0.1);
+                }
+            }
+            $("#body").remove();
+            $("#body2").attr("id", "body");
+            animate_speed = animate_speed_temp;
+            animating = false;
         }
     };
     const menu = () => {
         console.log("menu");
     };
+    $("body")[0].addEventListener("mousewheel", (e) => {
+        e = e || window.event;
+        e.wheelDelta = e.wheelDelta || e.detail;
+        if (e.wheelDelta > 0) {
+            prev();
+        }
+        if (e.wheelDelta < 0) {
+            next();
+        }
+    });
+    $("#prev")[0].addEventListener("click", (e) => {
+        prev();
+    });
+    $("#next")[0].addEventListener("click", (e) => {
+        next();
+    });
+    $("#menu")[0].addEventListener("click", (e) => {
+        menu();
+    });
+    $("#body > *").css("height", innerHeight - 50 + "px");
+    addEventListener("resize", (e) => {
+        $("#body > *").css("height", innerHeight - 50 + "px");
+    });
+    $("#body > div:not(#body > div:first-child)").hide();
 });
